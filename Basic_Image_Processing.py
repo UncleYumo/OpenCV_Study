940cff4da93b64b110640818957b146966c79dd9
uncle_yumo <wy13921971147@163.com>
2024-02-22 22:57:49 +0800

    gitignore隐藏测试2

--- Basic_Image_Processing.py
+++ Basic_Image_Processing.py
@@ -61,4 +61,4 @@ def proportional_scaling(name,img,SCALE):
 # cv_show(img_china)
 # cv_show("imag_gray",img_gray)
 
-#This is an useless sentence, to test the Branch Operate，and this sentence is from the remote repository! Hello Github
+#This is an useless sentence, to test the Branch Operate，and this sentence is from the remote repository!
